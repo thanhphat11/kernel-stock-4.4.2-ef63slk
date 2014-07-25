4e88e0383e3e24cad32d2dea57e91407bb602509
Jaegeuk Kim <jaegeuk@kernel.org>
2014-07-24 19:08:02 -0700

    f2fs: skip unnecessary data writes during fsync
    This patch intends to improve the fsync performance by skipping remaining the
    recovery information, only when there is no data that we should recover.
    
    Reviewed-by: Chao Yu <chao2.yu@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    
    Conflicts:
    	fs/f2fs/file.c

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -160,6 +160,17 @@ int f2fs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 		return ret;
 	}
 
+	/*
+	 * if there is no written data, don't waste time to write recovery info.
+	 */
+	if (!is_inode_flag_set(fi, FI_APPEND_WRITE) &&
+		!exist_written_data(sbi, inode->i_ino, APPEND_INO)) {
+		if (is_inode_flag_set(fi, FI_UPDATE_WRITE) ||
+			exist_written_data(sbi, inode->i_ino, UPDATE_INO))
+			goto flush_out;
+		goto out;
+	}
+
 	/* guarantee free sections for fsync */
 	f2fs_balance_fs(sbi);
 
@@ -239,11 +250,21 @@ int f2fs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 		if (ret)
 			goto out;
 <<<<<<< HEAD
+<<<<<<< HEAD
 		ret = blkdev_issue_flush(inode->i_sb->s_bdev, GFP_KERNEL, NULL);
 	}
 out:
 	mutex_unlock(&inode->i_mutex);
 =======
+=======
+
+		/* once recovery info is written, don't need to tack this */
+		remove_dirty_inode(sbi, inode->i_ino, APPEND_INO);
+		clear_inode_flag(fi, FI_APPEND_WRITE);
+flush_out:
+		remove_dirty_inode(sbi, inode->i_ino, UPDATE_INO);
+		clear_inode_flag(fi, FI_UPDATE_WRITE);
+>>>>>>> f144cff... f2fs: skip unnecessary data writes during fsync
 		ret = f2fs_issue_flush(F2FS_SB(inode->i_sb));
 	}
 out:
